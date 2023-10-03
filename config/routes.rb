7d7aa1a847a7340a609e96185b3389d54d9fbf88
haroonrana <haroonrana224@gmail.com>
2023-10-04 03:12:01 +0400

    added all login pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
         sessions: 'users/sessions'
+
       }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
