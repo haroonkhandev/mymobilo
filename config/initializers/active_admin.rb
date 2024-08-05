9b5e760f511a2ec4496344930c2699ab68f0f4b1
haroonrana <haroonrana224@gmail.com>
2024-08-05 15:01:53 +0400

    removed rails_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Wmd Solutions"
+  config.site_title = "MyMobilo"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
