9eaa5357b387c14c6e4d5a93c8105d17943a8633
haroonkhandev <haroonkhandev1@gmail.com>
2024-09-01 17:14:36 +0400

    updated ruby version 3.2.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if resource.shopkeeper?
-      shopkeepers_dashboard_path
-    elsif resource.user?
-      root_path # Updated to match your defined route
-    else
+    # if resource.shopkeeper?
+    #   shopkeepers_dashboard_path
+    # elsif resource.user?
+    #   root_path # Updated to match your defined route
+    # else
       root_path
-    end
+    # end
   end
 
   rescue_from CanCan::AccessDenied do |exception|
